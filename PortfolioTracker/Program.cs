21c39d498d3357c8baecd132649577317e2f9aa3
Valentin Iliescu <viliescu@hotmail.com>
2018-07-23 10:27:48 -0700

    Add project files.

---
+++ PortfolioTracker/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace PortfolioTracker
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine(HelloWorld());
+        }
+
+        [Pure]
+        private static string HelloWorld()
+        {
+            return "Hello " + "world!";
+        }
+    }
+}
---
+++ PortfolioTrackerTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PortfolioTrackerTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string.Empty.Should().BeEmpty();
+        }
+    }
+}
