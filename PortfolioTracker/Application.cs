d308f849b06fb5177b6b534b8d660a6d41e2360f
Valentin Iliescu <viliescu@hotmail.com>
2018-07-23 13:03:25 -0700

    Added the skeleton of the app: Application class with a single Render method that returns a hardcoded value.

---
+++ PortfolioTracker/Application.cs
@@ -0,0 +1,10 @@
+﻿namespace PortfolioTracker
+{
+    public class Application
+    {
+        public string Render()
+        {
+            return "You have 100 MSFT shares";
+        }
+    }
+}
---
+++ PortfolioTrackerTests/ApplicationTests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PortfolioTracker;
+
+namespace PortfolioTrackerTests
+{
+    [TestClass]
+    public class ApplicationTests
+    {
+        [TestMethod]
+        public void Render_should_return_the_hardcoded_value()
+        {
+            var application = new Application();
+            application.Render().Should().Be("You have 100 MSFT shares");
+        }
+    }
+}
