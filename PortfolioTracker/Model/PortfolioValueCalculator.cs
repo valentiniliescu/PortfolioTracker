a55bd276adb36434cdc04cb4c486514e035e801f
Valentin Iliescu <viliescu@hotmail.com>
2018-08-05 15:43:10 -0700

    Added portfolio value calculator

---
+++ PortfolioTracker/Model/PortfolioValueCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+
+namespace PortfolioTracker.Model
+{
+    public static class PortfolioValueCalculator
+    {
+        [Pure]
+        [MustUseReturnValue]
+        public static decimal Calculate([NotNull] Portfolio portfolio, [NotNull] [ItemNotNull] IEnumerable<Quote> quotes) =>
+            portfolio.Assets
+                .Join(quotes, asset => asset.Symbol, quote => quote.Symbol, (asset, quote) => quote != null ? asset.Amount * quote.Price : 0)
+                .Sum();
+    }
+}
