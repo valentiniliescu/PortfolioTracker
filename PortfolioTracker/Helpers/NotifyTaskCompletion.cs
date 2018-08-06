2e76299c9d24d2eaa316825e0c1e69fa5cd1b127
Valentin Iliescu <viliescu@hotmail.com>
2018-08-06 08:37:28 -0700

    Added NotifyTaskCompletion class

---
+++ PortfolioTracker/Helpers/NotifyTaskCompletion.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace PortfolioTracker.Helpers
+{
+    // from https://msdn.microsoft.com/en-us/magazine/dn605875.aspx
+    public sealed class NotifyTaskCompletion<TResult> : INotifyPropertyChanged
+    {
+        public NotifyTaskCompletion([NotNull] Task<TResult> task)
+        {
+            Task = task;
+            if (!task.IsCompleted)
+            {
+                Task _ = WatchTaskAsync(task);
+            }
+        }
+
+        [NotNull]
+        private Task<TResult> Task { get; }
+
+        public TResult Result => Task.Status == TaskStatus.RanToCompletion ? Task.Result : default(TResult);
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private async Task WatchTaskAsync([NotNull] Task task)
+        {
+            try
+            {
+                await task;
+            }
+            // ReSharper disable once EmptyGeneralCatchClause
+            catch
+            {
+            }
+
+            OnPropertyChanged(nameof(Result));
+        }
+
+        [NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
